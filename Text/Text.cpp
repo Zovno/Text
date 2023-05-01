036061d5de28eb319f3b0379dc193b37cccccfb6
Denho <Denho@DESKTOP-CEVI7C0>
2023-05-02 01:44:18 +0300

    я не могу больше

--- Text/Text.cpp
+++ Text/Text.cpp
@@ -12,6 +12,11 @@ typedef struct Word {
     struct Word* next;
 } Word;
 
+typedef struct Pair {
+    char* A;
+    char* B;
+} Pair;
+
 
 Word* createNode(const char* word) {
     Word* newNode = (Word*)malloc(sizeof(Word));
@@ -190,25 +195,150 @@ void displayWordOccurrences(const Word* head) {
     }
 }
 
+
+int isWordUsed(const Word* word, const Pair* pairs, int count) {
+    for (int i = 0; i < count; i++) {
+        if (strcmp(word->value, pairs[i].A) == 0 || strcmp(word->value, pairs[i].B) == 0) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+
+Pair* findWordPair(const Word* head, int* count) {
+    const Word* wordA = head;
+    const Word* wordB = head;
+    int maxScore = 0;
+    Pair* pairs = NULL;
+    int i = 0;
+
+    while (wordA != NULL) {
+        wordB = wordA->next;
+        while (wordB != NULL) {
+            int score = (wordA->count * strlen(wordA->value) + wordB->count * strlen(wordB->value)) -
+                (wordA->count * strlen(wordB->value) + wordB->count * strlen(wordA->value));
+
+            if (score > maxScore) {
+
+                if (!isWordUsed(wordA, pairs, i) && !isWordUsed(wordB, pairs, i)) {
+                    Pair* new_arr = (Pair*)realloc(pairs, (sizeof(Pair) * (*count + 1)));
+                    if (new_arr != NULL) {
+                        pairs = new_arr;
+                        (*count)++;
+                    }
+                    else {
+
+                        continue;
+                    }
+
+                    pairs[i].A = _strdup(wordA->value);
+                    pairs[i].B = _strdup(wordB->value);
+
+                    maxScore = score;
+                    i++;
+                }
+            }
+
+            wordB = wordB->next;
+        }
+
+        wordA = wordA->next;
+    }
+
+    if (maxScore == 0) {
+        printf("No word pair found.\n");
+    }
+
+    return pairs;
+}
+
+
+
+void printWordPairs(const Pair* pairs, int count) {
+    if (count == 0) {
+        printf("No word pairs found.\n");
+        return;
+    }
+
+    printf("Word pairs:\n");
+    for (int i = 0; i < count; i++) {
+        printf("Pair %d: %s - %s\n", i + 1, pairs[i].A, pairs[i].B);
+    }
+}
+
+
+void swapWordsInPair(Word* head, const Pair* pair) {
+    if (head == NULL || pair == NULL) {
+        return;
+    }
+
+    Word* current = head;
+    while (current != NULL) {
+        if (strcmp(current->value, pair->A) == 0) {
+            current->value = _strdup(pair->B);
+        }
+        else if (strcmp(current->value, pair->B) == 0) {
+            current->value = _strdup(pair->A);
+        }
+        current = current->next;
+    }
+}
+
+void writeListToFile(const Word* head, const char* filename) {
+    FILE* file = fopen(filename, "w");
+    if (file == NULL) {
+        printf("Error opening file: %s\n", filename);
+        return;
+    }
+
+    const Word* current = head;
+    while (current != NULL) {
+        fprintf(file, "%s", current->value);
+
+        if (current->next != NULL && ispunct(current->next->value[0])) {
+            if (current->next->next != NULL && ispunct(current->next->next->value[0])) {
+                fprintf(file, " ");
+            }
+        }
+        else {
+            fprintf(file, " ");
+        }
+
+        current = current->next;
+    }
+
+    fclose(file);
+}
+
 int main() {
 
 
     Word* head = loadWordsFromFile("D:\\text.txt");
     
 
-    displayList(head);
+    //displayList(head);
 
-    //Word* head2 = sortListByLength(head);
+
     
     Word* head3 = countWordOccurrences(head);
     
-    //displayList(head2);
+    //displayWordOccurrences(head3);
+    int* count = new int;
+    *(count) = 0;
 
-    displayWordOccurrences(head3);
+    Pair* pairs = findWordPair(head3, count);
 
+    printWordPairs(pairs, *count);
+    swapWordsInPair(head, pairs);
+    writeListToFile(head, "D:\\txt1.txt");
+    swapWordsInPair(head, pairs);
+    writeListToFile(head, "D:\\txt2.txt");
+    free(pairs);
+    free(count);
     freeList(head3);
-    //freeList(head2);
-    freeList(head);
 
+    freeList(head);
+    
     return 0;
 }
